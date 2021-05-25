661d0c6aab53f2cd79fd9797fe517aa254337414
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-05-25 19:16:48 -0400

    adding password digest to artist/user class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_200905) do
+ActiveRecord::Schema.define(version: 2021_05_25_231559) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "street_number"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_05_15_200905) do
     t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   create_table "concerts", force: :cascade do |t|
