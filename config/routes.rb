1f94199038bf38266a3335ce2f8180fd570d8ad8
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-05-26 20:36:48 -0400

    change concerts to nested route under artists and venues

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
 
   get "index", to: "static#index"
-  resources :addresses
-  resources :concerts
-  resources :venues
-  resources :artists
+  
+
+  resources :venues, only: [:show] do
+    resources :concerts
+  end
+
+  resources :artists, only: [:show] do
+    resources :concerts
+  end
+
+  resources :artists, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+  resources :venues, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
