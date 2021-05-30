43be44007d2f91c6cb1febe4a0c18ed53b98741b
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-05-30 19:12:44 -0400

    add create method for concert and add ability to select artist and venue

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -27,4 +27,20 @@ class ConcertsController < ApplicationController
     def new
         @concert = Concert.new
     end
+
+    def create
+        concert = Concert.new(concert_params)
+        if concert.save
+            redirect_to concert_path(concert)
+        else
+            render "new"
+        end
+    end
+
+
+    private
+
+    def concert_params
+        params.require(:concert).permit(:name, :description, :start_time, :end_time, :artist_id, :venue_id)
+    end
 end
