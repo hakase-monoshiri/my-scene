9212b4d23685e75a57d7fe5002cd06ae0432009d
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-05-30 18:39:23 -0400

    add index action for concerts controller

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,2 +1,22 @@
 class ConcertsController < ApplicationController
+
+    def index
+        if params[:artist_id]
+            artist = Artist.find_by(id: params[:artist_id])
+            if artist
+                @concerts = artist.concerts
+            else
+                redirect_to concerts_path
+            end
+        elsif params[:venue_id]
+            venue = Venue.find_by(id: params[:venue_id])
+            if venue
+                @concerts = venue.concerts
+            else
+                redirect_to concerts_path
+            end
+        else
+            @concerts = Concert.all
+        end
+    end
 end
