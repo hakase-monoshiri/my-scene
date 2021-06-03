7a98f1abd561dadb5eb3d17c0aa19e2d3eb12b10
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-06-03 00:53:01 -0400

    delete static controller and fix venue edit button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,17 @@ module ApplicationHelper
     def current_user_artist
         Artist.find_by(id: session[:user_id])
     end
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def login_logout_button
+        if logged_in?
+            link_to "Log-out", logout_path, method: :post
+        else
+            link_to "Log-in", login_path
+        end
+    end
+
 end
--- app/helpers/venues_helper.rb
+++ app/helpers/venues_helper.rb
@@ -2,7 +2,7 @@ module VenuesHelper
 
     def edit_venue_button(venue, artist)
         if artist.venues.include?(venue)
-            button_to "Edit Venue", edit_venue_path(venue)
+            button_to "Edit Venue", edit_venue_path(venue), method: :get
         end
     end
 
